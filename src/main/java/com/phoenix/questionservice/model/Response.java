f9335f980b41a552b6f564c27ce769267818c2b3
pratikagupta <pratika.gupta008@gmail.com>
2024-04-01 23:46:40 +0530

    Question service new project

---
+++ src/main/java/com/phoenix/questionservice/model/Response.java
@@ -0,0 +1,10 @@
+package com.phoenix.questionservice.model;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+}
---
+++ src/test/java/com/phoenix/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
